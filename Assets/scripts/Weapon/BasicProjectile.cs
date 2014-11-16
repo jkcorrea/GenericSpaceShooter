b5ced8b1e5afacbf9a35c613fc959c86b1e97dfa
AmyLiu11 <liuxiaofen11@gmail.com>
2014-11-15 20:40:21 -0500

    Basic Weapon interfaces
    Basic Weapon interfaces, projectile prefabs, test script can be
    replaced by player script

---
+++ Assets/scripts/Weapon/BasicProjectile.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicProjectile: MonoBehaviour{
+
+	public enum projectileType{
+		laser,
+		bullet
+	}
+
+	public projectileType pType;
+
+	public float damage;
+
+    GameObject enemy;
+
+	void Start() {
+		enemy = GameObject.FindGameObjectWithTag("enemy");
+	}
+
+	void OnCollisionEnter (Collision collision) {
+
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.blue);
+		}
+
+		// Apply damage to target object
+		enemy.collider.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);//"applyDamage" is the method enemy to call when projectile hit the enemy
+
+	}
+
+	void destoryProjectile(){
+		if (gameObject.tag.Equals ("bullet")) {
+			Destroy (gameObject, 4f);
+			Debug.Log (gameObject.name);
+		}
+	}
+
+	void Update(){
+		destoryProjectile ();
+	}
+
+
+}
