35ebdf1772b906f83cdd2b53b7f062cc9f2d93ec
Jake Correa <jcorrea257@gmail.com>
2014-11-20 01:07:52 -0500

    Touch controller

---
+++ Assets/scripts/PlayerController/TouchPlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TouchPlayerController : IPlayerController
+{
+    public override PlayerInputData getPlayerInputData()
+    {
+        var x = Input.acceleration;
+        PlayerInputData pid = new PlayerInputData();
+        pid.x = Input.acceleration.x;
+        pid.y = Input.acceleration.y;
+        foreach (Touch t in Input.touches) {
+            if (t.phase == TouchPhase.Began) {
+                pid.isFiring = true;
+                break;
+            }
+        }
+        return pid;
+    }
+}
