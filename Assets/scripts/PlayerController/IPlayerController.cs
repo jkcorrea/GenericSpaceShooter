2444a359363ded4c5b74f2870f85bffc70b7557f
Jake Correa <jcorrea257@gmail.com>
2014-11-19 20:50:43 -0500

    PCController done

---
+++ Assets/scripts/PlayerController/IPlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public abstract class IPlayerController : MonoBehaviour
+{
+    IShip ship;
+
+    void Start()
+    {
+        ship = GetComponent<IShip>();
+    }
+
+    void Update()
+    {
+        controlPlayerCharacter();
+    }
+        
+    protected void controlPlayerCharacter()
+    {
+        PlayerInputData pid = getPlayerInputData();
+        float x = pid.up - pid.down;
+        float y = pid.right - pid.left;
+        ship.Accelerate(new Vector3(x, y));
+    }
+
+    public abstract PlayerInputData getPlayerInputData();
+}
---
+++ Assets/scripts/PlayerController/PCPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    public class PCPlayerController : IPlayerController
+    {
+        public override PlayerInputData getPlayerInputData()
+        {
+            PlayerInputData pid = new PlayerInputData();
+
+            pid.up      = Input.GetKeyDown(KeyCode.UpArrow   ) ? 1f : -1f;
+            pid.down    = Input.GetKeyDown(KeyCode.DownArrow ) ? 1f : -1f;
+            pid.left    = Input.GetKeyDown(KeyCode.LeftArrow ) ? 1f : -1f;
+            pid.right   = Input.GetKeyDown(KeyCode.RightArrow) ? 1f : -1f;
+
+            return pid;
+        }
+    }
+}
